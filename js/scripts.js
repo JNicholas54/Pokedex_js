5a59238298303b43368b68682231a32ab4327622
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2022-07-17 20:20:46 -0700

    update css, showmodal function added

--- js/scripts.js
+++ js/scripts.js
@@ -37,6 +37,11 @@ let pokemonRepository = (function () {
         });
     };
 
+    function hideLoadingMessage() {
+        loadingMessage.remove();
+        console.log('All good, thanks for waiting!')
+    }
+
     function loadList() {
         return fetch(apiUrl).then(function (response) {
             return response.json();
@@ -58,14 +63,66 @@ let pokemonRepository = (function () {
         return fetch(url).then(function (response) {
             return response.json();
         }).then(function (details) {
+            hideLoadingMessage();
             // Now we add the details to the item
             item.imageUrl = details.sprites.frront_default;
             item.height = details.height;
             item.types = details.types;
         }).catch(function (e) {
+            hideLoadingMessage();
             console.error(e);
         });
     };
+
+    function showModal(title, text) {
+        let modalContainer = document.getElementById('modal-container');
+        modalContainer.innerHTML = '';   // Clear all existing modal content.
+        let modal = document.createElement('div');
+        modal.classList.add('model');
+
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('model-close'); // This class is in the CSS
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = title;
+
+        let contentElement = document.createElement('p');
+        contentElement.innerText = text;
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modalContainer.appendChild(modal); // modelCointainer is the father of modal, modal has 3 childs button, title and text.
+
+        modalContainer.classList.add('is-visible');
+    }
+
+    function hideModal() {
+        let modalContainer = document.getElementById('modal-container');
+        modalContainer.classList.remove('is-visible');
+    }
+
+    window.addEventListener('keydown', (e) => {
+        let modalContainer = document.getElementById('modal-container');
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+    });
+    let modalContainer = document.getElementById('modal-container');
+    modalContainer.addEventListener('click', (e) => {
+        // Since this is also triggered when clicking INSIDE the modal
+        // We only want to close if the user clicks directly on the overlay
+        let target = e.target;
+        if (target === modalContainer) {
+          hideModal();
+        }
+      });
+
+    document.getElementById('show-modal').addEventListener('click', () => {
+        showModal();
+    });
 
     return {
         add: add,
